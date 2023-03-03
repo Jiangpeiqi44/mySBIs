8d798252eaac2078d30ece81972b4f914d958b82
jiangpeiqi <1360755367@qq.com>
2023-03-03 19:59:37 +0800

    FIX IBI pick self BUG

--- src/utils/library/vit_gen_pcl.py
+++ src/utils/library/vit_gen_pcl.py
@@ -315,7 +315,7 @@ class BIOnlineGeneration():
             all_candidate_path = filter(
                 lambda k: k != background_face_path, all_candidate_path)
             all_candidate_path = list(all_candidate_path)
-            all_candidate_path = random.sample(self.ibi_data_list, k=4)
+            all_candidate_path = random.sample(all_candidate_path, k=4) # BUG HERE!
             all_candidate_path = ['{}_{}'.format(
                 vid_id, os.path.basename(i)) for i in all_candidate_path]
         else:

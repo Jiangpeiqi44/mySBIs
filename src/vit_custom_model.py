c523238825c6fddaf26226e7fe7ff27a78588cb9
jiangpeiqi <1360755367@qq.com>
2023-03-05 10:55:57 +0800

    hDRMLPv5-IMN

--- src/vit_custom_model.py
+++ src/vit_custom_model.py
@@ -182,11 +182,10 @@ class hDRMLPv5Embed(nn.Module):
         self.num_patches = num_patches
         self.hproj = torch.nn.Sequential(
             *[
-              nn.Conv2d(in_chans, embed_dim//16, kernel_size=3, stride=1, padding=1, bias=False), # [768,224,224] -> [768//16,224,224]
-              norm_layer(embed_dim//16),  # 这里采用BN，也可以采用LN
-              nn.GELU(),
-              
-              RegionLayerDW(embed_dim//16, embed_dim//16, (7,7)), #[768//16,224,224] -> [768//16,224,224]
+              nn.Conv2d(in_chans, embed_dim//16, kernel_size=3, stride=1, padding=1, bias=True), # [768,224,224] -> [768//16,224,224]
+
+              RegionLayerDWBNv2(embed_dim//16, embed_dim//16, (7,7)), #[768//16,224,224] -> [768//16,224,224]
+              norm_layer(embed_dim//16),
               nn.GELU(),
               
               nn.Conv2d(embed_dim//16, embed_dim//4, kernel_size=4, stride=4, bias=False), # [768//16,224,224] -> [768//4,56,56]
@@ -1458,7 +1457,45 @@ class Vit_consis_hDRMLPv4(nn.Module):
         x = self.custom_embed(x)
         cls_token, patch_token = self.vit_model(x)
         return cls_token
-    
+
+class Vit_hDRMLPv5_ImageNet(nn.Module):
+    def __init__(self, weight_pth=None):
+        super().__init__()
+        self.vit_model = vit_base_patch16_224_in21k_custom(
+            num_classes=100, has_logits=False, isEmbed=False, keepEmbedWeight=False, drop_ratio=0.1)
+        self.custom_embed = hDRMLPv5_embed(weight_pth)
+    def forward(self, x):
+        x = self.custom_embed(x)
+        cls_token, patch_token = self.vit_model(x)
+        return cls_token
+    def test_time(self, x):
+        return self.forward(x)
+
+class Vit_consis_hDRMLPv5(nn.Module):
+    def __init__(self, weight_pth=None):
+        super().__init__()
+        self.vit_model = vit_base_patch16_224_in21k_custom(
+            num_classes=2, has_logits=False, isEmbed=False, keepEmbedWeight=False)
+        self.custom_embed = hDRMLPv5_embed(weight_pth)
+        # consis-1
+        self.K = nn.Linear(768, 768)
+        self.Q = nn.Linear(768, 768)
+        self.scale = 768 ** -0.5
+    def forward(self, x):
+        x = self.custom_embed(x)
+        cls_token, patch_token = self.vit_model(x)
+        # # consis-1
+        consis_map = (self.K(patch_token) @
+                      self.Q(patch_token).transpose(-2, -1)) * self.scale
+        # # consis-2 add norm
+        # consis_map_norm = torch.norm(patch_token, p=2, dim=2, keepdim=True)
+        # consis_map = 0.5 + 0.5*((self.K(patch_token) @ self.Q(patch_token).transpose(-2, -1)) / (consis_map_norm@consis_map_norm.transpose(-2, -1)))
+        return cls_token, consis_map
+
+    def test_time(self, x):
+        x = self.custom_embed(x)
+        cls_token, patch_token = self.vit_model(x)
+        return cls_token
 if __name__ == '__main__':
     from torchinfo import summary
     model = Vit_local()

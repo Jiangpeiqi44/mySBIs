a007c854581e4affc629da22784beb98684dd10f
jiangpeiqi <1360755367@qq.com>
2023-03-24 12:31:08 +0800

    INT LINEAR

--- src/utils/MixBI_Map.py
+++ src/utils/MixBI_Map.py
@@ -265,7 +265,7 @@ class SBI_Dataset(Dataset):
                 #             ssim_map[16*i:16*(i+1), 16*j:16*(j+1)]).mean()
 
                 mask_f = cv2.resize(
-                    mask, (map_shape, map_shape), interpolation=cv2.INTER_AREA).astype('float32')
+                    mask, (map_shape, map_shape), interpolation=cv2.INTER_LINEAR).astype('float32')
 
                 mask_r = np.ones((196, 196),dtype='float32')
                 mask_f = self.Consistency2D(mask_f)  # ssim_patch，mask_f

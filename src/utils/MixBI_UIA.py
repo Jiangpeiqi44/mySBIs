1c2d75c0720f8af214a856d89ae64537b3a0ff89
jiangpeiqi <1360755367@qq.com>
2023-04-01 09:09:58 +0800

    DCv2-2

--- src/utils/MixBI_UIA.py
+++ src/utils/MixBI_UIA.py
@@ -274,15 +274,11 @@ class SBI_Dataset(Dataset):
                 mask_r = np.ones((196, 196),dtype='float32')
                 # #这里是x ray的相关性
                 # 非二值化
-                mask_x_ray_f = 4 * mask_f * (1 - mask_f)
+                # mask_x_ray_f = 4 * mask_f * (1 - mask_f)
                 # 二值化
-                # mask_x_ray_f = np.round(4 * mask_f * (1 - mask_f))
-                # mask_f = np.round(mask_f)
-                # if self.bi.not_aug_flag == True:
-                #     # IBI不增强的时候，内外脸是一致的
-                #     mask_f = mask_r
-                # else:
-                # #
+                mask_x_ray_f = np.round(4 * mask_f * (1 - mask_f))
+                mask_f = np.round(mask_f)
+                #
                 mask_f = self.Consistency2D(mask_f)  # ssim_patch，mask_f
                 mask_x_ray_f = self.Consistency2D(mask_x_ray_f)
                 flag = False
